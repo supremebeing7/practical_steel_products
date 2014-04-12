72209cfe4420f05d25fc8eecaad4026f943a307f
Mark Lehman & Nolan Miller <student@epicodus.com>
2014-04-11 17:06:49 -0700

    add error checking for unknown rows in csv upload

--- app/controllers/product_uploads_controller.rb
+++ app/controllers/product_uploads_controller.rb
@@ -4,7 +4,13 @@ class ProductUploadsController < ApplicationController
   end
 
   def create
-    @productUpload = ProductUpload.new(params[:file].tempfile).parse!
-    redirect_to products_path, notice: "Batch upload completed."
+    @product_upload = ProductUpload.new(params[:file].tempfile).parse!
+    if @product_upload.length > 0
+      column_names = @product_upload.first.keys
+      flash[:notice] = "Data in column(s) #{column_names.join(', ')} not added."
+    else
+      flash[:notice] = "Batch upload completed without errors."
+    end
+    redirect_to products_path
   end
 end
--- app/models/product_upload.rb
+++ app/models/product_upload.rb
@@ -4,9 +4,15 @@ class ProductUpload
   end
 
   def parse!
+    unknowns = []
     products_attributes = SmarterCSV.process(@file)
     products_attributes.each do |product_attributes|
+      throwaways = [:name, :description, :price]
+      unknown_attributes = product_attributes.reject { |k, v| throwaways.include? k }
+      unknowns << unknown_attributes
+      product_attributes.reject! { |k, v| !throwaways.include? k }
       Product.create(product_attributes)
     end
+    unknowns
   end
 end
